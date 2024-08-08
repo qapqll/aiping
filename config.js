bafcfb03f8b92073c29ed3b523e64981a555e800
qapqll <34290960+qapqll@users.noreply.github.com>
2024-08-09 00:05:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+config = {
+    generateStatistic: 'http://172.30.35.228:8081/link/Patw5L5i',
+    recommendStatistic: 'http://172.30.35.228:8081/link/gKBi98Up',
+    wsUrl: 'ws://172.29.100.13:8083/script/assistant/socket'
+}
